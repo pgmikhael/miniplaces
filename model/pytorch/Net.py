923b0f4b5b9368a1e55477621a56cdeb62bee7b5
pgmikhael <pgmikhael@gmail.com>
2019-04-07 00:06:03 -0400

    alexnet cnn

--- model/pytorch/Net.py
+++ model/pytorch/Net.py
@@ -1,75 +1,132 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-class Net(nn.Module):
+"""
+TODO:
+    - load data
+    - train net code
+    - test net code
+    - compute accuracies: acc1_total /= num_batch; acc5_total /= num_batch
+"""
 
+
+# Dataset Parameters
+batch_size = 200
+load_size = 256
+fine_size = 224
+c = 3
+data_mean = np.asarray([0.45834960097,0.44674252445,0.41352266842])
+
+# Training Parameters
+learning_rate = 0.001
+dropout = 0.5 # Dropout, probability to keep units
+training_iters = 100000
+step_display = 50
+step_save = 10000
+path_save = 'alexnet'
+start_from = ''
+
+
+class Net(nn.Module):
+    """
     def __init__(self):
         super(Net, self).__init__()
-        # 1 input image channel, 6 output channels, 5x5 square convolution
-        # kernel
-        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        # an affine operation: y = Wx + b
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
+    """
+    def __init__(self, num_classes=1000):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
 
     def forward(self, x):
-        # Max pooling over a (2, 2) window
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        # If the size is a square you can only specify a single number
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = x.view(-1, self.num_flat_features(x))
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.features(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        x = self.classifier(x)
         return x
 
-    def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
-
-
-
-"""
-When we call loss.backward(), the whole graph is differentiated w.r.t. 
-the loss, and all Tensors in the graph that has requires_grad=True will 
-have their .grad Tensor accumulated with the gradient.
-"""
 
 net = Net()
-params = list(net.parameters())
-# create your optimizer
-optimizer = optim.SGD(net.parameters(), lr=0.01)
-criterion = nn.MSELoss()
+criterion = nn.CrossEntropyLoss()                                           # define loss
+optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9)     # define optimization alg
 
-input = torch.randn(1, 1, 32, 32)
-output = net(input)
 
-net.zero_grad()
-output.backward(torch.randn(1, 10))
-
-optimizer.zero_grad()   # zero the gradient buffers
-
-target = torch.randn(10)  # a dummy target, for example
-target = target.view(1, -1)  # make it the same shape as output
-
-loss = criterion(output, target)
-loss.backward()
-optimizer.step()  
+# Load Data
 
 
 
+# Train the network
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)               # forward prop
+        loss = criterion(outputs, labels)   # compute loss
+        loss.backward()                     # back prop
+        optimizer.step()                    # update weights
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
 
 
 
+# Test the network
 
+outputs = net(images)
+_, predicted = torch.max(outputs, 1)
 
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                              for j in range(4)))
 
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
-
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
