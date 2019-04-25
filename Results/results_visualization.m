f48e0f9325bccb9b17d55a3b76d5474b2784a018
pgmikhael <pgmikhael@gmail.com>
2019-04-25 11:27:10 -0400

    updated matlab file

--- Results/results_visualization.m
+++ Results/results_visualization.m
@@ -7,7 +7,6 @@ figure(1)
 subplot(3,1,1)
 plot(training(:,1), training(:,2), '-r', validation(:,1), validation(:,2), '-g');
 legend('Training Loss', 'Validation Loss', 'Location','southwest');
-savefig('Loss.fig')
 
 subplot(3,1,2)
 plot(training(:,1), training(:,3), '-r', validation(:,1), validation(:,3), '-g');
@@ -19,4 +18,4 @@ plot(training(:,1), training(:,4), '-r', validation(:,1), validation(:,4), '-g')
 ylim([0,1]);
 legend('Training Acc5', 'Validation Acc5', 'Location','northwest');
 
-saveas(1, 'results2.png','png');
+saveas(1, 'results.png','png');
